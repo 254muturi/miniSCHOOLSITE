3e823edfdc090d3cbf9c431df6ce21f04a8ae69d
254muturi <mdmuturi254@gmail.com>
2023-08-21 09:34:41 +0300

    SCHOOLSITE 002 COMMIT

--- main.py
+++ main.py
@@ -1,37 +1,78 @@
 import os
 from datetime import datetime
+from flask_admin import Admin
 from flask_wtf import FlaskForm
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
-from wtforms.validators import DataRequired
-from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Length
+from flask_admin.contrib.sqla import ModelView
 from flask import Flask, render_template, flash, request
+from wtforms import StringField, SubmitField, FileField, PasswordField
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.secret_key = "NJOROGE MUTURI MW@NGI 1st@254"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# SQLite DATABASE
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Mastude.db')
-
 
 # MySQL Database
-# NEW MySQL DB
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:daniel1st$mwangi@localhost/FadhiliDB"
+app.secret_key = "NJOROGE MUTURI MW@NGI 1st@254"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
+admin = Admin()
+
+admin.init_app(app)
+
 
 # create a Model
-class Student(db.Model):
+class Student(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    Class_Teacher = db.Column(db.String(100), nullable=False, unique=True)
     name = db.Column(db.String(100), nullable=False)
+    grade = db.Column(db.String(100), nullable=False)
+    parent_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     phone_number = db.Column(db.String(100))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<Name {self.student_name}>'
+        return '<Name %r>' % self.name
+
+
+admin.add_view(ModelView(Student, db.session))
+
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = SchoolDataBaseValidationForm()
+    if form.validate_on_submit():
+        user_data = Student.query.filter_by(email=form.email.data).first()
+
+        if user_data is None:
+            # PASSWORD HASHING
+            user_data = Student(Class_Teacher=form.Class_Teacher.data,
+                                name=form.name.data, grade=form.grade.data,
+                                parent_name=form.parent_name.data,
+                                email=form.email.data, phone_number=form.phone_number.data)
+            db.session.add(user_data)
+            db.session.commit()
+
+        name = form.name.data
+        form.Class_Teacher.data = ''
+        form.name.data = ''
+        form.grade.data = ''
+        form.parent_name.data = ''
+        form.email.data = ''
+        form.phone_number.data = ''
+
+        flash("Student Added Successfully")
+    our_users = Student.query.order_by(Student.date_added)
+    return render_template("add_user.html", form=form, name=name, our_users=our_users)
+
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -53,18 +94,28 @@ def delete(id):
 
 
 class SchoolDataBaseValidationForm(FlaskForm):
+    Class_Teacher = StringField(' Class Teacher ', validators=[DataRequired()])
+    password_hash = PasswordField('Password',
+                                  validators=[DataRequired(), EqualTo('Password_hash2', message='Password Must Match')])
+    password_hash2 = PasswordField('Confirm  Password', validators=[DataRequired()])
     name = StringField(' Name ', validators=[DataRequired()])
+    grade = StringField(' Class ', validators=[DataRequired()])
+    parent_name = StringField(' Parent Name ', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
-    phone_number = StringField("Phone Number",validators=[DataRequired()])
+    phone_number = StringField("Phone Number", validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# UPDATE DATABASE RECORD
+    # UPDATE DATABASE RECORD
+
+
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     form = SchoolDataBaseValidationForm()  # Corrected the colon ':' here
     name_to_update = Student.query.get_or_404(id)
     if request.method == 'POST':
         name_to_update.name = request.form["name"]
+        name_to_update.grade = request.form["grade"]
+        name_to_update.parent_name = request.form["parent_name"]
         name_to_update.email = request.form["email"]
         name_to_update.phone_number = request.form["phone_number"]
 
@@ -82,34 +133,18 @@ def update(id):
     else:
         return render_template("update.html",
                                form=form,
-                               name_to_update=name_to_update)
+                               name_to_update=name_to_update, id=id)
 
 
 class SchoolValidationForm(FlaskForm):
-    name = StringField('Full Name ', validators=[DataRequired()])
+    password_hash = PasswordField('Password ', validators=[DataRequired(),
+                                                           EqualTo('Password_hash2', message='Passwords  Must Match')])
+    password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
+    name = StringField('Student Name ', validators=[DataRequired()])
+    parent_name = StringField('Parent Name ', validators=[DataRequired()])
+    phone_number = StringField('Phone Number  ', validators=[DataRequired()])
+
     submit = SubmitField('Submit')
-
-
-@app.route('/user/add', methods=['GET', 'POST'])
-def add_user():
-    name = None
-    form = SchoolDataBaseValidationForm()
-    if form.validate_on_submit():
-        user = Student.query.filter_by(email=form.email.data).first()
-
-        if user is None:
-            user = Student(name=form.name.data,
-                           email=form.email.data, phone_number = form.phone_number.data)
-            db.session.add(user)
-            db.session.commit()
-
-        name = form.name.data
-        form.name.data = ''
-        form.email.data = ''
-        form.phone_number.data = ''
-        flash("Student Added Successfully")
-    our_users = Student.query.order_by(Student.date_added)
-    return render_template("add_user.html", form=form, name=name, our_users=our_users)
 
 
 @app.route('/')
@@ -129,8 +164,43 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
-        flash(" Submitted Successfully")
+        flash(" Admission Request Submitted Successfully Patiently Wait For Our Response")
     return render_template("name.html", name=name, form=form)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/students')
+def students():
+    return render_template('students.html')
+
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('contact_us.html')
+
+
+@app.route('/parents_and_guardians')
+def parents_and_guardians():
+    return render_template('parents_and_guardians.html')
+
+
+@app.route('/admissions')
+def admissions():
+    return render_template('admissions.html')
+
+
+@app.route('/how_to_help')
+def how_to_help():
+    return render_template('how_to_help.html')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 
 @app.errorhandler(404)
